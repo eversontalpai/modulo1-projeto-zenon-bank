6eb22811d2e07892837e57c434d31bfe84622b74
eversontalpai <eversontalpai@gmail.com>
2026-09-12 19:18:28 -0300

    feat: refatora para usar o reduce

--- zenon-fraud-detector/src/main/java/br/com/zenon/report/TransactionReport.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/report/TransactionReport.java
@@ -5,31 +5,35 @@ import br.com.zenon.reader.CsvReaderWithStream;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
 public class TransactionReport {
-    private Long totalLines = 0L;
-    private Long totalFrauds = 0L;
-    private BigDecimal totalAmount = BigDecimal.ZERO;
+    private final Statistics statistics;
+
+    private record Statistics(Long totalLines, Long totalFrauds, BigDecimal totalAmount){
+        private final static Statistics ZERO = new Statistics(0L,0L,BigDecimal.ZERO);
+        private  Statistics add(Transaction transaction){
+            return new Statistics(totalLines()+1L,
+                    totalFrauds()+(transaction.isFraud()?1L:0L),
+                    totalAmount.add(transaction.amount()) );
+
+        }
+        private Statistics sum(Statistics other){
+            return new Statistics(totalLines +other.totalLines(),
+                    totalFrauds +other.totalFrauds(),
+                    other.totalAmount.add(other.totalAmount) );
+
+        }
+    }
 
     public TransactionReport(String fileName) {
 
-        try(Stream<String> transactions =CsvReaderWithStream.readCsv(fileName)){
-
-            transactions
+        try(Stream<String> transactions = CsvReaderWithStream.readCsv(fileName)){
+           this.statistics = transactions
                     .map(Transaction::creatTransaction)
                     .filter(Objects::nonNull)
-                    .forEach(transaction -> {
-
-                this.totalLines ++;
-                if(transaction.isFraud()){
-                    this.totalFrauds ++;
-
-                }
-                this.totalAmount = this.totalAmount.add(transaction.amount());
-            });
+                    .reduce(Statistics.ZERO, Statistics::add, Statistics::sum);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -37,9 +41,9 @@ public class TransactionReport {
 
 
     public void printReport(){
-        IO.println("Total de linhas: "+totalLines);
-        IO.println("Total de fraudes: "+totalFrauds);
-        IO.println("Valor total transacionado: %.2f".formatted(totalAmount));
+        IO.println("Total de linhas: "+this.statistics.totalLines());
+        IO.println("Total de fraudes: "+this.statistics.totalFrauds());
+        IO.println("Valor total transacionado: %.2f".formatted(this.statistics.totalAmount));
     }
 
 
